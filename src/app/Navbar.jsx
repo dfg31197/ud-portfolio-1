f890a0ea8c215033559162d871398583ba1badb9
Samarth Kamboj <thirteen@Samarths-MacBook-Air.local>
2025-07-21 21:03:16 +0530

    nav fix

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
             <a href="#contact">Contact</a>
           </li>
         </ul>
-        <div onClick={() => toggleNav(true)} className="flex cursor-pointer">
-          <div className="bg-primary sm:hidden px-2 rounded flex flex-col gap-1 justify-center">
+        <div  className="flex cursor-pointer">
+          <div onClick={() => toggleNav(true)} className="bg-primary sm:hidden px-2 rounded flex flex-col gap-1 justify-center">
             <div className="bg-white h-0.5 w-6 rounded"></div>
             <div className="bg-white h-0.5 w-3.5 mt-0.5 rounded"></div>
           </div>
@@ -35,19 +35,19 @@ const Navbar = () => {
           </a>
         </div>
         {isNavToggled && <div onClick={() => toggleNav(false)} className="w-[100%] h-[100%] bg-primary text-white fixed top-0 left-0 right-0 bottom-0">
-          <ul>
-            <li className="text-right text-3xl">x</li>
+          <ul className="*:text-center">
+            <li className="text-right text-3xl p-2">x</li>
                     <li>
-            <a href="#">Home</a>
+            <a className="p-4 block w-[100%]" href="#">Home</a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a className="p-4 block w-[100%]" href="#about">About</a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a className="p-4 block w-[100%]" href="#services">Services</a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a className="p-4 block w-[100%]" href="#contact">Contact</a>
           </li>
           </ul>
           </div>}
